a09867e9c85b73ac8928ebfd4dfb28578329bd36
soasme <soasme@gmail.com>
2014-09-11 09:49:27 +0800

    Check expected response for interact.

--- src/beanstalk_clj/core.clj
+++ src/beanstalk_clj/core.clj
@@ -10,12 +10,21 @@
 (def ^:const default-ttr 120)
 (def ^:const crlf (str \return \newline))
 
+(defn member? [list elt]
+  "True if list contains at least one instance of elt"
+  (cond
+   (empty? list) false
+   (= (first list) elt) true
+   true (member? (rest list) elt)))
 
 (defprotocol Interactive
   (close [this] "Close the connection")
   (read [this] "Read from connection")
   (write [this data] "Write data to connection")
-  (interact [this command]))
+  (interact [this command expected_ok])
+  (interact-yaml [this command])
+  (interact-job [this command])
+  (interact-peek [this command]))
 
 
 (deftype Beanstalkd [socket reader writer]
@@ -49,18 +58,25 @@
      (.flush)))
 
   (interact
-   [this command]
+   [this command expected_ok]
    (write this command)
-   (let [data (read this)]
-     (string/split data #" "))))
+   (let [bin (read this)
+         data (string/split bin #" ")
+         resp (first data)]
+     (cond
+      (member? expected_ok resp)
+      data
+
+      true
+      (throw+ {:type :unexpected-response :message bin})))))
 
 
 (defn beanstalkd-factory
   ([]
    (beanstalkd-factory default-host default-port))
   ([config]
-  (let [[host port_str] (string/split config #":")
-        port (Integer/parseInt port_str)]
+  (let [[host port-str] (string/split config #":")
+        port (Integer/parseInt port-str)]
     (beanstalkd-factory host port)))
   ([host port]
    (let [socket (java.net.Socket. host port)]
@@ -100,4 +116,4 @@
 
 (defn watch
   [beanstalkd tube]
-  (interact beanstalkd (beanstalkd-cmd :watch tube)))
+  (interact beanstalkd (beanstalkd-cmd :watch tube) ["WATCHING"]))
