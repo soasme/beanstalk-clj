4ac9238a85dc1822d61f0284611180d24167e9d1
soasme <soasme@gmail.com>
2014-09-11 08:43:08 +0800

    Define consts and with scope.

--- src/beanstalk_clj/core.clj
+++ src/beanstalk_clj/core.clj
@@ -1,6 +1,27 @@
-(ns beanstalk-clj.core)
+(ns beanstalk-clj.core
+  (:use [slingshot.slingshot :only [throw+]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:const default-host "localhost")
+(def ^:const default-port 11300)
+(def ^:const default-priority 2147483648) ; 2^31
+(def ^:const default-ttr 120)
+(def ^:const crlf (str \return \newline))
+
+
+(def beanstalkd-factory
+  [config])
+
+(defn- with-beanstalkd*
+  [f]
+  (fn [beanstalkd & rest :as args]
+    (if (and (thread-bound? *beanstalkd*)
+             (not (identical? *beanstalkd* beanstalkd)))
+      (f *beanstalkd* args)
+      (f beanstalkd rest)))
+
+(defmacro with-beanstalkd
+  "Takes a url an config. That value is used to configure the subject
+   of all of the operations within the dynamic scope of body of code."
+  [config & body]
+  `(binding [*beanstalkd* (beanstalkd-factory config)]
+     ~@body))
