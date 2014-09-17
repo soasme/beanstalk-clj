7bd213b60d58ccac3034ed04f872e3c00a2c6733
soasme <soasme@gmail.com>
2014-09-17 09:57:50 +0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beanstalk-clj "0.1.0-SNAPSHOT"
+(defproject beanstalk-clj "0.1.0"
   :description "A simple beanstalkd client library for Clojure."
   :url "http://github.com/soasme/beanstalk-clj"
   :license {:name "Eclipse Public License"
